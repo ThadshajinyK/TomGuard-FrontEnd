989006b7493d7187d800f31bd7c01eccdff0f018
ThadshajinyK <kaneshanthadshajiny@gmail.com>
2023-06-23 01:57:20 +0530

    changes in something

--- front-end/src/Alert_page/AlertService.js
+++ front-end/src/Alert_page/AlertService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const ALERT_API_BASE_URL = "http://localhost:8080/api/v1/alerts";
+const ALERT_API_BASE_URL = "http://localhost:9090/api/v1/alerts";
 
 class AlertService {
   getAlerts() {
