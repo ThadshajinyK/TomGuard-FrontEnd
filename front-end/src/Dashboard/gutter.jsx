0c8bbe73d3666b6d520caf1bab0398ff9d4f3dca
Ragavarshithan <105372507+Ragavarshithan@users.noreply.github.com>
2023-06-24 00:22:37 +0530

    Update gutter.jsx

--- front-end/src/Dashboard/gutter.jsx
+++ front-end/src/Dashboard/gutter.jsx
@@ -1,69 +1,84 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import axios from 'axios'
-import { useState,useEffect } from "react";
-
+import axios from 'axios';
 
 function Gutters() {
-    const [servers, setserver] = useState([]);
+  const [servers, setServers] = useState([]);
 
-    useEffect(() => {
-      loadserver();
-    }, []);
-  
-    const loadserver = async () => {
-      try {
-        const response = await axios.get("http://localhost:3000/users");
-        setserver(response.data);
-      } catch (error) {
-        console.error('Error occurred while loading users:', error);
-      }
-    };
-    return (
-        <div class="container mt-3">
-            {servers.map((server)=>(
-            <div class="row ">
-                <div class="col-md-2">
-                   <div class="p-3 border border-dark shadow shadow-lg rounded bg-light m-2" style={{ "height": "85%" }}>Servers<br/><br></br><b>{server.server}</b></div>
-                </div>
-                <div class="col-md-2">
-                    <div class="p-3 border rounded bg-light m-2" style={{ "height": "85%" }}>Apps<br/><br></br><b>{server.app}</b></div>
-                </div>
-                <div class="col-md-4">
-                    <div class="p-3 border rounded bg-light m-2" >
-                        <div className="row">
-                            <div className="col-md-8">Response Time :</div>
-                            <div className="col-md-4"><b>{server.response}s</b></div>
-                        </div>
-                    </div>
-                    <div class="p-3 border rounded bg-light m-2">
-                        <div className="row">
-                            <div className="col-md-8">No Of Session :</div>
-                            <div className="col-md-4"><b>{server.Noofsession}</b></div>
-                        </div>
-                    </div>
-                </div>
-                
-                <div class="col-md-4">
-                    <div class="p-3 border rounded bg-light m-2">
-                        <div className="row">
-                            <div className="col-md-8">Thread Usage :</div>
-                            <div className="col-md-4"><b>{server.Thread}</b></div>
-                        </div>
-                    </div>
-                    <div class="p-3 border rounded bg-light m-2">
-                        <div className="row">
-                            <div className="col-md-8">Up Time :</div>
-                            <div className="col-md-4"><b>{server.uptime}s</b></div>
-                        </div>
-                    </div>
-                </div>
+  useEffect(() => {
+    loadServerCount();
+  }, []);
 
+  const loadServerCount = async () => {
+    try {
+      const response = await axios.get("http://localhost:9090/server/servercount");
+      setServers(response.data);
+    } catch (error) {
+      console.error('Error occurred while loading server count:', error);
+    }
+  };
 
-            </div>))}
+  const [apps, setApps] = useState([]);
+
+  useEffect(() => {
+    loadappCount();
+  }, []);
+
+  const loadappCount = async () => {
+    try {
+      const response = await axios.get("http://localhost:9090/apps/appcount");
+      setApps(response.data);
+    } catch (error) {
+      console.error('Error occurred while loading app count:', error);
+    }
+  };
+
+  return (
+    <div className="container mt-3">
+      <div className="row">
+        <div className="col-md-2">
+          <div className="p-3 border border-dark shadow shadow-lg rounded bg-light m-2" style={{ height: "85%" }}>
+            Servers<br /><br />
+            <b>{servers}</b>
+          </div>
         </div>
-    );
+        <div className="col-md-2">
+          <div className="p-3 border rounded bg-light m-2" style={{ height: "85%" }}>
+            Apps<br /><br />
+            <b>{apps}</b>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="p-3 border rounded bg-light m-2">
+            <div className="row">
+              <div className="col-md-8">Response Time:</div>
+              <div className="col-md-4"><b>{/* Add response time value here */}s</b></div>
+            </div>
+          </div>
+          <div className="p-3 border rounded bg-light m-2">
+            <div className="row">
+              <div className="col-md-8">No Of Session:</div>
+              <div className="col-md-4"><b>{/* Add session count here */}</b></div>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-4">
+          <div className="p-3 border rounded bg-light m-2">
+            <div className="row">
+              <div className="col-md-8">Thread Usage:</div>
+              <div className="col-md-4"><b>{/* Add thread usage value here */}</b></div>
+            </div>
+          </div>
+          <div className="p-3 border rounded bg-light m-2">
+            <div className="row">
+              <div className="col-md-8">Up Time:</div>
+              <div className="col-md-4"><b>{/* Add uptime value here */}s</b></div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Gutters;
-
