92d9d16b7e82cd4f8c44e031ffc893837e65aec9
Ragavarshithan <105372507+Ragavarshithan@users.noreply.github.com>
2023-06-26 00:11:34 +0530

    gutter completely connected with back end(except no of session)

--- front-end/src/Dashboard/gutter.jsx
+++ front-end/src/Dashboard/gutter.jsx
@@ -2,13 +2,15 @@ import React, { useState, useEffect } from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import axios from 'axios';
 
-function Gutters() {
+function Gutters({ hostName }) {
   const [servers, setServers] = useState([]);
- 
+  const [apps, setApps] = useState([]);
+  const [data, setData] = useState([]);
+  
+
 
   useEffect(() => {
     loadServerCount();
-    
   }, []);
 
   const loadServerCount = async () => {
@@ -19,8 +21,6 @@ function Gutters() {
       console.error('Error occurred while loading server count:', error);
     }
   };
-
-  const [apps, setApps] = useState([]);
 
   useEffect(() => {
     loadappCount();
@@ -34,55 +34,86 @@ function Gutters() {
       console.error('Error occurred while loading app count:', error);
     }
   };
- 
-    
- 
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const serverResponse = await axios.get('http://localhost:9090/server/all');
+  //       setData1(serverResponse.data);
+  //     } catch (error) {
+  //       console.error('Error fetching server data:', error);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const serverResponse = await axios.get('http://localhost:9090/metrics/all');
+        setData(serverResponse.data);
+      } catch (error) {
+        console.error('Error fetching server data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  
+
+  // Retrieve the lastly added server's uptime
+  const lastUptime = data.length > 0 ? data[data.length - 1].uptimeInMillis : null;
+  const lastResponsetime = data.length > 0 ? data[data.length - 1].responseTimeInMillis : null;
+  const lastRequesttime = data.length > 0 ? data[data.length - 1].requestTimeInMillis : null;
 
   return (
     <div className="container mt-3">
-      <div className="row mx-5">
-        <div className="col-md-2 ">
-          <div className="p-3 border rounded bg-light m-2 " style={{ height: "85%" }}>
-            Servers<br /><br />
-            <b>{servers}</b>
-          </div>
+  <div className="row mx-5">
+    <div className="col-md-2 ">
+      <div className="p-3 border rounded bg-light m-2 " style={{ height: "85%" }}>
+        Servers<br /><br />
+        <b>{servers}</b>
+      </div>
+    </div>
+    <div className="col-md-2">
+      <div className="p-3 border rounded bg-light m-2" style={{ height: "85%" }}>
+        Apps<br /><br />
+        <b>{apps}</b>
+      </div>
+    </div>
+    <div className="col-md-4">
+      <div className="p-3 border rounded bg-light m-2">
+        <div className="row">
+          <div className="col-md-6">Response Time:</div>
+          <div className="col-md-6"><b>{lastResponsetime ? `${lastResponsetime} ms` : ''}</b></div>
         </div>
-        <div className="col-md-2">
-          <div className="p-3 border rounded bg-light m-2" style={{ height: "85%" }}>
-            Apps<br /><br />
-            <b>{apps}</b>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="p-3 border rounded bg-light m-2">
-            <div className="row">
-              <div className="col-md-8">Response Time:</div>
-              <div className="col-md-4"><b>{/* Add response time value here */}s</b></div>
-            </div>
-          </div>
-          <div className="p-3 border rounded bg-light m-2">
-            <div className="row">
-              <div className="col-md-8">No Of Session:</div>
-              <div className="col-md-4"><b>{/* Add session count here */}</b></div>
-            </div>
-          </div>
-        </div>
-        <div className="col-md-4">
-          <div className="p-3 border rounded bg-light m-2">
-            <div className="row">
-              <div className="col-md-8">Request time:</div>
-              <div className="col-md-4"><b>{/* Add Request time value here */}</b></div>
-            </div>
-          </div>
-          <div className="p-3 border rounded bg-light m-2">
-            <div className="row">
-              <div className="col-md-8">Up Time:</div>
-              <div className="col-md-4"><b>{/* Add Up Time value here */}s</b></div>
-            </div>
-          </div>
+      </div>
+      <div className="p-3 border rounded bg-light m-2">
+        <div className="row">
+          <div className="col-md-6">No Of Session:</div>
+          <div className="col-md-6"><b>{/* Add session count here */}</b></div>
         </div>
       </div>
     </div>
+    <div className="col-md-4">
+      <div className="p-3 border rounded bg-light m-2">
+        <div className="row">
+          <div className="col-md-6">Request time:</div>
+          <div className="col-md-6"><b>{lastRequesttime ? `${lastRequesttime} ms` : ''}</b></div>
+        </div>
+      </div>
+      <div className="p-3 border rounded bg-light m-2">
+        <div className="row">
+          <div className="col-md-6">Up Time:</div>
+          <div className="col-md-6"><b>{lastUptime ? `${lastUptime} ms` : ''}</b></div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
+
   );
 }
 
